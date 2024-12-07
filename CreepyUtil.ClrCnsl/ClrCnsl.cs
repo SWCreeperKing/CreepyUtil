0d558adb9bbb9d5e81c3e960af507d28b48fd3f6
SWCreeperKing <SW.CreeperKing@gmail.com>
2024-12-06 18:02:12 -0800

    ConsoleColor -> Ascii

--- CreepyUtil.ClrCnsl/ClrCnsl.cs
+++ CreepyUtil.ClrCnsl/ClrCnsl.cs
@@ -1,14 +1,27 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 using static System.ConsoleKey;
+using static CreepyUtil.ClrCnsl.AsciiEffects;
 
 namespace CreepyUtil.ClrCnsl;
 
 public static class ClrCnsl
 {
-    public const char Block = '\u2588';
+    // public const char Block = '\u2588';
     public const int ListLeng = 11;
-    public static readonly Regex RegMatch = new(@"\[(#|!)(\w+?)\]", RegexOptions.Compiled);
+
+    public static readonly Regex RegMatch = new(@"\[(?:(#|!)([a-zA-Z]+?|\d{1,3},\d{1,3},\d{1,3})|(@)(\w+?))\]",
+        RegexOptions.Compiled);
+
+    public static bool UseAscii;
+
     private static readonly string[] SourceArray = ["^", "v"];
+
+    public static void EnableAscii()
+    {
+        Console.OutputEncoding = Encoding.ASCII;
+        UseAscii = true;
+    }
 
     public static int ListView(int selection = 0, params string[] options)
     {
@@ -84,6 +97,12 @@ public static class ClrCnsl
             return;
         }
 
+        if (UseAscii)
+        {
+            WriteAscii(text);
+            return;
+        }
+
         Stack<ConsoleColor> foregroundColorStack = new();
         Stack<ConsoleColor> backgroundColorStack = new();
         foregroundColorStack.Push(ConsoleColor.White);
@@ -102,6 +121,8 @@ public static class ClrCnsl
             var isBack = match.Groups[1].Value == "!";
 
             Console.Write(txt[index..nIndex].ToString());
+
+            if (match.Groups[3].Value == "@") continue;
             var rawValue = match.Groups[2].Value;
             ConsoleColor? color = rawValue == "r" ? null : Enum.Parse<ConsoleColor>(rawValue, true);
 
@@ -131,19 +152,111 @@ public static class ClrCnsl
         void PeekColor() { SetColors(foregroundColorStack.Peek(), backgroundColorStack.Peek()); }
     }
 
+    private static void WriteAscii(string text)
+    {
+        AsciiReset();
+        Stack<Color> foregroundColorStack = new();
+        Stack<Color> backgroundColorStack = new();
+        var lastForeground = Color.White;
+        var lastBackground = Color.Black;
+
+        Span<char> txt = text.ToCharArray();
+        var index = 0;
+
+        while (RegMatch.IsMatch(txt, index))
+        {
+            var match = RegMatch.Match(text, index);
+            var matchValue = match.Value;
+            var nIndex = text.IndexOf(matchValue, index, StringComparison.Ordinal);
+            var isBack = match.Groups[1].Value == "!";
+
+            Console.Write(txt[index..nIndex].ToString());
+
+            var rawValue = match.Groups[2].Value.ToLower();
+            if (match.Groups[3].Value == "@")
+            {
+                AsciiSet(match.Groups[4].Value.ToLower() switch
+                {
+                    "r" => ResetNormal,
+                    "b" => BoldOrIncreaseIntensity,
+                    "/b" => BoldOffOrDoubleUnderline,
+                    "i" => Italic,
+                    "/i" => NotItalic,
+                    "u" => Underline,
+                    "uu" => BoldOffOrDoubleUnderline,
+                    "/u" or "/uu" => UnderlineOff,
+                    "blink" => RapidBlink,
+                    "/blink" => BlinkOff,
+                    "o" => Overlined,
+                    "/o" => NotOverlined,
+                    "s" => CrossOut,
+                    "/s" => NotCrossOut,
+                    "swap" => SwapFgAndBgColor,
+                    "/swap" => SwapFgAndBgColorOff,
+                    _ => NA
+                });
+            }
+            else
+            {
+                switch (isBack)
+                {
+                    case true when rawValue == "r" && backgroundColorStack.Count > 1:
+                        backgroundColorStack.Pop();
+                        break;
+                    case true when rawValue != "r":
+                        backgroundColorStack.Push(rawValue.ToColor());
+                        break;
+                    default:
+                        if (rawValue == "r" && foregroundColorStack.Count > 1) foregroundColorStack.Pop();
+                        else if (rawValue != "r") foregroundColorStack.Push(rawValue.ToColor());
+                        break;
+                }
+            }
+
+            PeekColor();
+            index = nIndex + matchValue.Length;
+        }
+
+        if (index < txt.Length) Console.Write(txt[index..].ToString());
+
+        AsciiReset();
+        return;
+
+        void PeekColor()
+        {
+            Color nextFg;
+            Color nextBg;
+            if (CanSetColor(nextFg = NextColor(foregroundColorStack, Color.White), lastForeground))
+            {
+                AsciiSetFgColorRgb(nextFg);
+                lastForeground = nextFg;
+            }
+
+            if (CanSetColor(nextBg = NextColor(backgroundColorStack, Color.Black), lastBackground))
+            {
+                AsciiSetBgColorRgb(nextBg);
+                lastBackground = nextBg;
+            }
+        }
+
+        Color NextColor(Stack<Color> colorStack, Color defColor)
+        {
+            return colorStack.Count == 0 ? defColor : colorStack.Peek();
+        }
+
+        bool CanSetColor(Color nextColor, Color lastColor) { return nextColor != lastColor; }
+    }
+
     public static void SetColors(ConsoleColor? foreground = null, ConsoleColor? background = null)
     {
         if (foreground is not null) Console.ForegroundColor = foreground.Value;
-
         if (background is not null) Console.BackgroundColor = background.Value;
     }
 
     public static void WaitForSpaceInput()
     {
         WriteLine("\nPress the Spacebar to continue . . . ");
-        while (GetKey() != Spacebar)
-        {
-        }
+        while (GetKey() != Spacebar) ;
     }
 
     public static void WaitForAnyInput()
@@ -179,7 +292,7 @@ public static class ClrCnsl
         var segmentsUsed = (int)(segmentCount * delta);
         if (segmentsUsed == 0 && delta > 0) segmentsUsed = 1;
 
-        Write($"[#{getColor(delta)}][{Block.Repeat(segmentsUsed)}{' '.Repeat(segmentCount - segmentsUsed)}][#r]");
+        Write($"[[!{getColor(delta)}]{' '.Repeat(segmentsUsed)}[!r]{' '.Repeat(segmentCount - segmentsUsed)}]");
         return delta;
     }
 
@@ -195,17 +308,11 @@ public static class ClrCnsl
     }
 
     public static void Write(char c) { Console.Write(c); }
-
     public static void Write(object s) { Write(s.ToString()); }
-
     public static void WriteLine(string s) { Write($"{s}\n"); }
-
     public static void WriteLine(object s) { Write($"{s}\n"); }
-
     public static void WriteLine(char c) { Console.WriteLine(c); }
-
     public static ConsoleKey GetKey() { return Console.ReadKey(true).Key; }
-
     public static string CleanColors(string text) { return RegMatch.Replace(text, string.Empty); }
 
     public static (ConsoleColor, ConsoleColor) GetColors()
@@ -214,14 +321,94 @@ public static class ClrCnsl
     }
 
     public static Pos GetCursor() { return Console.GetCursorPosition(); }
-
     public static void SetCursor(int x = 0, int y = 0) { Console.SetCursorPosition(x, y); }
-
     public static void SetCursor(Pos pos) { Console.SetCursorPosition(pos.X, pos.Y); }
-
     public static void CursorVis(bool condition = true) { Console.CursorVisible = condition; }
-
     public static void Clr() { Console.Clear(); }
+    public static void AsciiReset() { AsciiSet(ResetNormal); }
+
+    public static void AsciiSet(AsciiEffects id)
+    {
+        if (id == NA) return;
+        Console.Write($"\e[{(int)id}m");
+    }
+
+    public static void AsciiSetFgColorRgb(string color) { Console.Write($"\e[38;2;{color}m"); }
+
+    public static void AsciiSetBgColorRgb(string color) { Console.Write($"\e[48;2;{color}m"); }
+    public static void AsciiSetFgColorRgb(Color color) { Console.Write($"\e[38;2;{color.R};{color.G};{color.B}m"); }
+    public static void AsciiSetBgColorRgb(Color color) { Console.Write($"\e[48;2;{color.R};{color.G};{color.B}m"); }
+}
+
+public enum AsciiEffects
+{
+    NA = -1,
+    ResetNormal = 0,
+    BoldOrIncreaseIntensity = 1,
+    FaintOrDecreaseIntensity = 2,
+    Italic = 3,
+    Underline = 4,
+    SlowBlink = 5,
+    RapidBlink = 6,
+    SwapFgAndBgColor = 7,
+    Conceal = 8,
+    CrossOut = 9,
+    PrimaryFont = 10,
+    AltFont1 = 11,
+    AltFont2 = 12,
+    AltFont3 = 13,
+    AltFont4 = 14,
+    AltFont5 = 15,
+    AltFont6 = 16,
+    AltFont7 = 17,
+    AltFont8 = 18,
+    AltFont9 = 19,
+    BoldOffOrDoubleUnderline = 21,
+    NormalColorOrIntensity = 22,
+    NotItalic = 23,
+    UnderlineOff = 24,
+    BlinkOff = 25,
+    SwapFgAndBgColorOff = 27,
+    ConcealOff = 28,
+    NotCrossOut = 29,
+    FgBlack = 30,
+    FgRed = 31,
+    FgGreen = 32,
+    FgYellow = 33,
+    FgBlue = 34,
+    FgMagenta = 35,
+    FgCyan = 36,
+    FgWhite = 37,
+    SetFgColor = 38,
+    DefaultFgColor = 39,
+    BgBlack = 40,
+    BgRed = 41,
+    BgGreen = 42,
+    BgYellow = 43,
+    BgBlue = 44,
+    BgMagenta = 45,
+    BgCyan = 46,
+    BgWhite = 47,
+    SetBgColor = 48,
+    DefaultBgColor = 49,
+    Overlined = 53,
+    NotOverlined = 55,
+    FgBrightBlack = 90,
+    FgBrightRed = 91,
+    FgBrightGreen = 92,
+    FgBrightYellow = 93,
+    FgBrightBlue = 94,
+    FgBrightMagenta = 95,
+    FgBrightCyan = 96,
+    FgBrightWhite = 97,
+    BgBrightBlack = 100,
+    BgBrightRed = 101,
+    BgBrightGreen = 102,
+    BgBrightYellow = 103,
+    BgBrightBlue = 104,
+    BgBrightMagenta = 105,
+    BgBrightCyan = 106,
+    BgBrightWhite = 107,
 }
 
 public static class Helper
---
+++ CreepyUtil.ClrCnsl/Color.cs
@@ -0,0 +1,84 @@
+﻿using System.Globalization;
+using static CreepyUtil.ClrCnsl.ColorExt;
+
+namespace CreepyUtil.ClrCnsl;
+
+public readonly record struct Color
+{
+    private static readonly Dictionary<string, Color> ParseCache = [];
+    public static readonly Color Black = new(0, 0, 0);
+    public static readonly Color White = new(255, 255, 255);
+
+    public readonly byte R;
+    public readonly byte G;
+    public readonly byte B;
+
+    public Color(byte r, byte g, byte b)
+    {
+        R = r;
+        G = g;
+        B = b;
+    }
+
+    public Color(string text)
+    {
+        if (ParseCache.TryGetValue(text, out this)) return;
+        if (ColorDict.TryGetValue(text, out var hex))
+        {
+            this = HexToColor(hex);
+            return;
+        }
+
+        if (IsHexColor(text))
+        {
+            this = HexToColor(text);
+            return;
+        }
+
+        if (text.Contains(','))
+        {
+            var split = text.Split(',');
+            if (split.Length != 3)
+            {
+                this = White;
+                return;
+            }
+
+            if (!byte.TryParse(split[0], out R)) R = 255;
+            if (!byte.TryParse(split[1], out G)) G = 255;
+            if (!byte.TryParse(split[2], out B)) B = 255;
+            ParseCache[text] = this;
+            return;
+        }
+
+        Console.WriteLine(
+            $"Failed to parse: [{text}] | not in chache, not in color dictionary, not hex, not ###,###,###");
+        this = White;
+    }
+
+    public static bool IsHexColor(string hex)
+    {
+        return hex.Length == 6 && hex.All(c => c is >= 'a' and <= 'f' or >= '0' and <= '9' or >= 'A' and <= 'F');
+    } 
+    
+    public static Color HexToColor(string hex)
+    {
+        if (IsHexColor(hex))
+        {
+            var r = byte.Parse(hex[..2], NumberStyles.HexNumber);
+            var g = byte.Parse(hex[2..4], NumberStyles.HexNumber);
+            var b = byte.Parse(hex[4..6], NumberStyles.HexNumber);
+            Color color = new(r, g, b);
+            ParseCache[hex] = color;
+            return color;
+        }
+
+        Console.WriteLine($"Failed to parse, [{hex}] is not hex");
+        return White;
+    }
+
+    public static implicit operator Color(string text) => new(text);
+    public static implicit operator string(Color color) => color.ToString();
+
+    public override string ToString() { return $"{R},{G},{B}"; }
+}
