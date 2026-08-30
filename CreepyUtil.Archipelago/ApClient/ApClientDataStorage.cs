9f21f6994a021b262bd5ac187276e070eae16218
SWCreeperKing <sw.creeperking@gmail.com>
2026-08-30 01:22:22 -0700

    improve slot data storage

--- CreepyUtil.Archipelago/ApClient/ApClientDataStorage.cs
+++ CreepyUtil.Archipelago/ApClient/ApClientDataStorage.cs
@@ -9,16 +9,30 @@ public partial class ApClient
 {
     public event Action<Exception>? OnDataStorageListenerError;
     private Dictionary<int, DataStorageHelper.DataStorageUpdatedHandler> CoreDsListeners = [];
-    private Dictionary<int, HashSet<DataStorageHelper.DataStorageUpdatedHandler>> DsListeners = [];
+    private Dictionary<int, Dictionary<string, DataStorageHelper.DataStorageUpdatedHandler>> DsListeners = [];
 
-    public void AddDataStorageListener(string key, DataStorageHelper.DataStorageUpdatedHandler action,
-        Scope scope = Scope.Global)
+    public bool ContainsDataStorageListener(string key, string functionId, Scope scope = Scope.Global)
+    {
+        var id = (key, scope).GetHashCode();
+        if (!CoreDsListeners.ContainsKey(id)) return false;
+        return DsListeners.TryGetValue(id, out var s) && s.ContainsKey(functionId);
+    }
+
+    public void AddDataStorageListener(string key, string functionId,
+        DataStorageHelper.DataStorageUpdatedHandler action, Scope scope = Scope.Global)
     {
         try
         {
             var id = (key, scope).GetHashCode();
-            if (DsListeners.ContainsKey(id)) DsListeners[id].Add(action);
-            else DsListeners[id] = [action];
+            if (!DsListeners.ContainsKey(id)) DsListeners[id] = [];
+            if (DsListeners[id].ContainsKey(functionId))
+            {
+                OnDataStorageListenerError?.Invoke(
+                    new ArgumentException($"Key Function [{functionId}] already assigned to [{key}]")
+                );
+                return;
+            }
+            DsListeners[id][functionId] = action;
 
             if (CoreDsListeners.ContainsKey(id)) return;
             DataStorageHelper.DataStorageUpdatedHandler listener = (o, n, d) => OnValueChanged(id, o, n, d);
@@ -28,13 +42,16 @@ public partial class ApClient
         catch (Exception e) { OnDataStorageListenerError?.Invoke(e); }
     }
 
-    public void RemoveDataStorageListeners(string key, Scope scope = Scope.Global)
+    public void RemoveDataStorageListeners(string key, string functionId, Scope scope = Scope.Global)
     {
         try
         {
             var id = (key, scope).GetHashCode();
-            DsListeners.Remove(id);
+            if (!DsListeners.TryGetValue(id, out var dsListeners)) return;
+            if (!dsListeners.Remove(functionId)) return;
+            if (DsListeners[id].Count != 0) return;
             if (!CoreDsListeners.TryGetValue(id, out var listener)) return;
+
             Session!.DataStorage[key].OnValueChanged -= listener;
             CoreDsListeners.Remove(id);
         }
@@ -45,7 +62,7 @@ public partial class ApClient
         Dictionary<string, JToken> additionalArguments)
     {
         if (!DsListeners.TryGetValue(id, out var listener)) return;
-        foreach (var action in listener)
+        foreach (var action in listener.Values)
         {
             try { action.Invoke(originalValue, newValue, additionalArguments); }
             catch (Exception e) { OnDataStorageListenerError?.Invoke(e); }
